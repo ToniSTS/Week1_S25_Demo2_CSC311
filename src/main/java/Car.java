6ec34aa50ceb6ad8126ddba7bf4472a29bd3e166
Jeantonio Stsurin <ironswing88@gmail.com>
2025-01-29 10:09:50 -0500

    adding class

---
+++ src/main/java/Car.java
@@ -0,0 +1,9 @@
+public class Car {
+ Vehicle Info = new Vehicle();
+ String brand;
+
+ public void String(){
+    System.out.println("Honk, honk");
+ }
+ public
+}
---
+++ src/main/java/Vehicle.java
@@ -0,0 +1,10 @@
+public class Vehicle {
+    public static int id ;{
+    int number0fWheels;
+    String color;
+    float engineSize;
+    String fuelType;
+}
+
+
+}
