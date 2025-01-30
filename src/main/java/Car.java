b92c34379f52d8567b5dbd6240cd632060de93a9
Jeantonio Stsurin <ironswing88@gmail.com>
2025-01-29 21:24:21 -0500

    displayInfo changes

--- src/main/java/Car.java
+++ src/main/java/Car.java
@@ -1,12 +1,16 @@
 public class Car extends Vehicle {
+    public Car(int number0fwheels, String color, float engineSize, String fuelType){
+        super(number0fwheels, color, engineSize, fuelType);
+        this.brand = brand;
+    }
 
  String brand;
 
  public void Honk(){
     System.out.println("Honk, honk");
  }
- public void displayInfo(){
-     System.out.println("Brand " + brand );
+ public void displayInfo() {
+     System.out.println("Brand " + brand);
      System.out.println("Number of wheels " + getNumber0fWheels());
      System.out.println("Color " + getColor());
      System.out.println("Engine Size " + getEngineSize());
--- src/main/java/Vehicle.java
+++ src/main/java/Vehicle.java
@@ -1,41 +1,47 @@
 public class Vehicle {
+    int number0fWheels;
+    String color;
+    float engineSize;
+    String fuelType;
 
-        int number0fWheels;
-        String color;
-        float engineSize;
-        String fuelType;
+    public Vehicle(int number0fWheels, String color, float engineSize,String fuelType) {
+        this.number0fWheels = number0fWheels;
+        this.color = color;
+        this.engineSize = engineSize;
+        this.fuelType = fuelType;
 
-    public int setNumber0fWheels () {
-        return number0fWheels;
+    }
+
+
+        public int setNumber0fWheels() {
+            return number0fWheels;
         }
 
-        public int getNumber0fWheels(int number0fwheels){
-            this.number0fWheels = number0fwheels;
-            return number0fwheels;
+        public int getNumber0fWheels(){
+            return number0fWheels;
         }
 
         public String setColor() {
         return color;
         }
 
-        public String getColor(String Color){
-        this.color = color;
-        return color;
+        public String getColor(){
+            return color;
         }
 
         public float setEngineSize(){
             return engineSize;
         }
-        public float getEngineSize(float engineSize){
-            this.engineSize = engineSize;
+
+        public float getEngineSize(){
             return engineSize;
         }
 
         public String setFuelType() {
             return fuelType;
         }
+
         public String getFuelType(){
-            this.fuelType = fuelType;
             return fuelType;
         }
 
